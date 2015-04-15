9fae0f051967c7a52b86c6d7c5902b54b045f479
Alex Hancock <alex@alexhancock.com>
2015-04-15 11:42:46 -0700

    [apps] Add docker and virtualbox

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,10 @@ function install_deps() {
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
     # Install cli tools
-    brew install vim node python htop postgres redis speedtest_cli caskroom/cask/brew-cask
+    brew install vim node python htop postgres redis speedtest_cli docker boot2docker caskroom/cask/brew-cask
     pip install awscli
     # Install apps
-    brew cask install google-chrome google-drive dropbox charles 1password iterm2 alfred sizeup vlc evernote
+    brew cask install alfred sizeup google-chrome google-drive dropbox charles 1password iterm2 virtualbox vlc evernote
 }
 
 function doIt() {
